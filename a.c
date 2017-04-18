0fef2e0463256fae6ffa240cd751e7040deab0fc
ARJUN <arjunamb96@gmail.com>
2017-04-18 15:54:53 +0530

    modified

--- a.c
+++ a.c
@@ -1,14 +1,1 @@
-1234
-567
-wetz
-aa
-
-adaf
-modified on 29 march 2017
-modified again from client
-<<<<<<< HEAD
-kkkkkk
-=======
-jkhkjhkjhkj
->>>>>>> 823e196c724592a64ef7e936c818b4e1013b676a
-gfdgbfb
+hloooooooo its meeeeeeeee
